152ebb7304940be4bb0bffcee1b5453a15dfcba8
Andrii Poizzhan <achellas88@gmail.com>
2022-07-10 22:50:40 +0300

    en

---
+++ resources/lang/en/main.php
@@ -0,0 +1,56 @@
+<?php
+
+return [
+    'name' => 'Name',
+    'email' => 'Email',
+    'hide' => 'Hide',
+    'show' => 'Show',
+    'save' => 'Save',
+    'categories' => 'Categories',
+    'category' => 'Category',
+    'select_category' => 'Select category',
+    'all_category' => 'All categories',
+    'popular_category' => 'Popular categories',
+
+    'countries' => 'All countries',
+    'country' => 'Country',
+    'select_country' => 'Select country',
+
+    'city' => 'City',
+    'input_city' => 'Input your city',
+
+    'language' => 'Language',
+    'select_language' => 'Select your language',
+
+    'search' => 'Search',
+    'recommended' => 'Recommended',
+    'most_popular' => 'Most popular',
+    'last_reviews' => 'Last reviews',
+
+
+    //profile
+    'main_info' => 'Main info',
+    'address' => 'Address',
+    'input_address' => 'Input your address',
+    'languages' => 'Languages',
+    'contacts' => 'Contacts info',
+    'avatar' => 'Profile photo',
+    'change_avatar' => 'Change profile photo',
+    'upload' => 'Upload',
+    'state' => 'State',
+    'input_state' => 'Input your state',
+    'langlist_text' => 'Add languages you speak here',
+    'add_language' => 'Add language',
+    'profile' => 'Profile',
+    'add_review' => 'Leave feedback',
+    'contacts_text' => 'Add your contact details to contact you',
+    'phone' => 'Phone',
+    'viber' => 'Viber',
+    'telegram' => 'Telegram',
+    'whatsapp' => 'Whatsapp',
+    'facebook' => 'Facebook',
+    'instagram' => 'Instagram',
+    'services' => 'Services',
+    'add_service' => 'Add service',
+    'speaking' => 'Spoken languages',
+];
---
+++ resources/lang/en/navs.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'sign_in' => 'Sign in',
+    'sign_up' => 'Sign Up',
+    'logout' => 'Logout',
+
+    //Menu
+    'home' => 'Home',
+    'contacts' => 'Contacts',
+    'about_us' => 'About Us',
+    'profile' => 'Profile',
+    'search' => 'Search',
+];
